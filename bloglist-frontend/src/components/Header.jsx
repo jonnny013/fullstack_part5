e50dcaabdc8272964db2d68faf1a479f10f35f08
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-10-31 11:33:37 +0800

    Add css media queries

--- bloglist-frontend/src/components/Header.jsx
+++ bloglist-frontend/src/components/Header.jsx
@@ -10,16 +10,13 @@ const Header = (props) => {
     justifyContent: 'space-between',
     borderRadius: 5,
   }
-  const logo = {
-    width: 100,
-    height: 'auto',
-    paddingLeft: 50,
-  }
+
   return (
-    <div style={styles} >
-      <img className='logo' style={logo} src={logoPic} alt='logo' />
+    <div style={styles} className='headerDiv'>
+      <img className='logo'  src={logoPic} alt='logo' />
       <h1 className='blogTitle'>Blogs</h1>
-      <p className='userInfo'>{props.user.name} is logged in. <button id='logout-button' onClick={props.handleLogout}>Logout</button></p>
+      <p className='userInfo'>{props.user.name} is logged in.</p>
+      <button id='logout-button' onClick={props.handleLogout}>Logout</button>
     </div>
   )
 }
